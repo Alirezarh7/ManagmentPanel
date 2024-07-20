b873b21a8652b16ae8b4e8272655670530a8e7da
Alirezarh7 <alireza.ir76@gmail.com>
2024-07-20 10:37:43 +0330

    add some API in content.const.ts

--- src/constants/content.const.ts
+++ src/constants/content.const.ts
@@ -3,5 +3,6 @@ import { IGeneralEnumWithFaTitle } from '../typs/general.types';
 export const contentLocations: IGeneralEnumWithFaTitle[] = [
 	{ name: 'Payment', nameFa: 'پرداخت', id: 1 },
 	{ name: 'Register', nameFa: 'ثبت نام', id: 2 },
-	{ name: 'Abort', nameFa: 'انصراف', id: 3 }
+	{ name: 'Abort', nameFa: 'انصراف', id: 3 },
+	{ name: 'CommitmentLetter', nameFa: 'تعهدنامه', id: 4 }
 ];
