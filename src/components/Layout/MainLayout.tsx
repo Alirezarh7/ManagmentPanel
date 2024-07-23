f273485ca1994c1a22e0c7964149666872fe6388
MamadTaheri68 <mamad.taheri.68@gmail.com>
2024-07-23 15:03:33 +0330

    feat: Add husky and linter

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,35 +1,49 @@
-import React, { ComponentType, useState } from 'react';
+import React, { ComponentType, useState } from "react";
 
-import WebRoute from '../../router/WebRoute';
-import { IDashboardState } from '../../pages/PublicPages/Actions/Dashboard/model';
-import { dashboardActions } from '../../pages/PublicPages/Actions/Dashboard/action';
-import { IApplicationState } from '../../store/state';
-import { connect } from 'react-redux';
-import NapAlerts from '../general/NapAlerts/NapAlerts';
-import Sidebar from './SideBar';
-import Header from './Header';
+import WebRoute from "../../router/WebRoute";
+import { IDashboardState } from "../../pages/PublicPages/Actions/Dashboard/model";
+import { dashboardActions } from "../../pages/PublicPages/Actions/Dashboard/action";
+import { IApplicationState } from "../../store/state";
+import { connect } from "react-redux";
+import NapAlerts from "../general/NapAlerts/NapAlerts";
+import Sidebar from "./SideBar";
+import Header from "./Header";
 
-type IProps = typeof dashboardActions & IDashboardState & { isConnected: boolean };
+type IProps = typeof dashboardActions &
+  IDashboardState & { isConnected: boolean };
 
 const MainLayout = (props: IProps) => {
-	const [isOpenSidebar, setIsOpenSidebar] = useState(false);
+  const [isOpenSidebar, setIsOpenSidebar] = useState(false);
 
-	return (
-		<div className='bg-boxdark-2 text-black dark:text-bodydark  '>
-			<div className='flex h-screen overflow-hidden'>
-				<Sidebar sidebarOpen={isOpenSidebar} setSidebarOpen={setIsOpenSidebar} />
-				<div className='relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden'>
-					<Header sidebarOpen={isOpenSidebar} setSidebarOpen={setIsOpenSidebar} isConnected={props.isConnected} />
-					<main>
-						<div className='mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10'>
-							<WebRoute />
-							<NapAlerts alerts={props.alerts} clearAlerts={() => props.clearAlerts()} />
-						</div>
-					</main>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="dark:bg-boxdark-2 text-black dark:text-bodydark">
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar
+          sidebarOpen={isOpenSidebar}
+          setSidebarOpen={setIsOpenSidebar}
+        />
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+          <Header
+            sidebarOpen={isOpenSidebar}
+            setSidebarOpen={setIsOpenSidebar}
+            isConnected={props.isConnected}
+          />
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              <WebRoute />
+              <NapAlerts
+                alerts={props.alerts}
+                clearAlerts={() => props.clearAlerts()}
+              />
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default connect((state: IApplicationState) => state.dashboard, dashboardActions)(MainLayout as ComponentType<any>);
+export default connect(
+  (state: IApplicationState) => state.dashboard,
+  dashboardActions,
+)(MainLayout as ComponentType<any>);
--- src/components/general/ToggleButton/NapToggleButton.tsx
+++ src/components/general/ToggleButton/NapToggleButton.tsx
@@ -1,30 +1,39 @@
-import React from 'react';
-import "./NapToggleButton.css"
+import React from "react";
+import "./NapToggleButton.css";
+
 interface IProps {
-    value: boolean
-    onChangeActivate: () => any
+  value: boolean;
+  onChangeActivate: () => any;
 }
-export default class NapToggleButton extends React.Component<IProps>{
-    state = {
-        isActive: false
+
+export default class NapToggleButton extends React.Component<IProps> {
+  state = {
+    isActive: false,
+  };
+
+  componentDidMount() {
+    if (this.props.value) {
+      this.setState({ isActive: this.props.value });
     }
-    componentDidMount() {
-        if(this.props.value){
-            this.setState({isActive: this.props.value})
-        }
-    }
-    onChangeHandler = () => {
-        this.setState({isActive: !this.state.isActive })
-        setTimeout(() => {
-            this.props.onChangeActivate()
-        }, 250);
-    }
-    render() {
-        return (
-            <label className="switch">
-                <input type="checkbox" checked={this.state.isActive} onChange={this.onChangeHandler}/>
-                    <span className="slider round"></span>
-            </label>
-                )
-            }
+  }
+
+  onChangeHandler = () => {
+    this.setState({ isActive: !this.state.isActive });
+    setTimeout(() => {
+      this.props.onChangeActivate();
+    }, 250);
+  };
+
+  render() {
+    return (
+      <label className="switch">
+        <input
+          type="checkbox"
+          checked={this.state.isActive}
+          onChange={this.onChangeHandler}
+        />
+        <span className="slider round"></span>
+      </label>
+    );
+  }
 }
