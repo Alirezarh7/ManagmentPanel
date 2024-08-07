cfdd8a878b1959b3a8be240104589fe0944b209b
MamadTaheri68 <mamad.taheri.68@gmail.com>
2024-08-07 15:31:30 +0330

    feat: work on urls

--- src/shareData/baseUrls.ts
+++ src/shareData/baseUrls.ts
@@ -1,24 +1,40 @@
-const baseUrls = {
-	Interanl: 'https://apigateway.navoshgaran.com/ManagementPanel/api',
-	Sadad: 'https://my.haj.ir/api',
+export const currentBaseUrl = process.env.REACT_APP_BASEURL;
+export const currentPaymentBaseUrl = process.env.REACT_APP_PAYMENT_BASEURL;
+export const managementPanelBaseUrl = process.env.REACT_APP_MANAGEMENT_BASEURL;
+export const managementPanelCreatPerson = process.env.REACT_APP_CREATE_PERSON_URL;
+//
+export const ssoURLGov = process.env.REACT_APP_SSO_GOV_URL;
+export const ssoURLOrganization = process.env.REACT_APP_SSO_ORGANIZATION_URL;
 
-	PaymentInternal: 'https://apigateway.navoshgaran.com/ManagementPanel/api',
-	PaymentSadad: 'https://my.haj.ir'
-};
+// old ************************************************************************
+// *
+// *
+// *
+// const baseUrls = {
+// 	Interanl: 'https://apigateway.navoshgaran.com/ManagementPanel/api',
+// 	Sadad: 'https://my.haj.ir/api',
+//
+// 	PaymentInternal: 'https://apigateway.navoshgaran.com/ManagementPanel/api',
+// 	PaymentSadad: 'https://my.haj.ir'
+// };
+//
+// const managementBaseUrls = {
+// 	navoshgaran: 'https://apigateway.navoshgaran.com/ManagementPanel/api',
+// 	sadad: 'my.haj.ir/api/ManagementPanel/api'
+// };
+//
+// const creatPersonToken = {
+// 	creatPesron: 'https://apigateway.navoshgaran.com/BasicInfo/api/People/CreatePerson'
+// };
 
-const managementBaseUrls = {
-	navoshgaran: 'https://apigateway.navoshgaran.com/ManagementPanel/api',
-	sadad: 'my.haj.ir/api/ManagementPanel/api'
-};
-
-const creatPersonToken = {
-	creatPesron: 'https://apigateway.navoshgaran.com/BasicInfo/api/People/CreatePerson'
-};
-
-export const currentBaseUrl: string = baseUrls.Interanl;
-export const currentPaymentBaseUrl: string = baseUrls.PaymentInternal;
-export const managementPanelBaseUrl: string = managementBaseUrls.navoshgaran;
-export const managementPanelCreatPerson: string = creatPersonToken.creatPesron;
-
-export const ssoURLGov = 'https://sso.my.gov.ir/oauth2';
-export const ssoURLOrganization = 'https://auth.haj.ir/';
+// export const currentBaseUrl: string = baseUrls.Interanl;
+// export const currentPaymentBaseUrl: string = baseUrls.PaymentInternal;
+// export const managementPanelBaseUrl: string = managementBaseUrls.navoshgaran;
+// export const managementPanelCreatPerson: string = creatPersonToken.creatPesron;
+//
+// export const ssoURLGov = 'https://sso.my.gov.ir/oauth2';
+// export const ssoURLOrganization = 'https://auth.haj.ir/';
+// *
+// *
+// *
+// old ************************************************************************
