ae5c8cf4ab6b40c3ad6adbe8073655bf4fdc2e15
a.rahimi <alireza.ir76@gmail.com>
2024-12-17 10:24:55 +0330

    edit layaout and make main annoincemnetTable responsive

--- src/components/announcement/AnnouncementsTable.tsx
+++ src/components/announcement/AnnouncementsTable.tsx
@@ -14,16 +14,16 @@ const AnnouncementsTable = ({ announcements, announcementsByMainPage }: IProps) 
 	const selectedSourceToShow = showType === 'all' ? announcements : announcementsByMainPage;
 
 	return (
-		<div className='rounded-md border !border-stroke bg-white md:px-4 md:py-4 space-y-2 shadow-default dark:border-strokedark dark:bg-boxdark'>
+		<div className='rounded-md border !border-stroke bg-white md:py-4 space-y-2 shadow-default dark:border-strokedark dark:bg-boxdark'>
 			<div className='flex items-center gap-4'>
 				<CustomButton variant={'secondary'} type={'button'} label={'همه'} onClick={() => setShowType('all')} />
 				<CustomButton variant={'secondary'} type={'button'} label={'صفحه اصلی'} onClick={() => setShowType('mainPage')} />
 			</div>
-			<div className='max-w-full overflow-x-auto'>
+			<div className='max-w-full'>
 				<table className='w-full table-auto'>
 					<thead>
 						<tr className='bg-gray-100 text-right dark:bg-meta-4'>
-							<th className='w-[50px] py-3 px-1 text-center font-medium text-black dark:text-white'>آی دی</th>
+							<th className='max-md:hidden w-[50px] py-3 px-1 text-center font-medium text-black dark:text-white'>آی دی</th>
 							<th className='py-3 px-1 w-[110px] text-center font-medium text-black dark:text-white'>تصویر</th>
 							<th className='min-w-[200px] max-w-[200px] py-3 px-3 font-medium text-black dark:text-white'>موضوع اطلاعیه</th>
 							{showType === 'all' ? (
@@ -34,7 +34,7 @@ const AnnouncementsTable = ({ announcements, announcementsByMainPage }: IProps) 
 									صفحه اصلی
 								</th>
 							) : null}
-							<th className='w-[120px] py-3 px-1 text-center font-medium text-black dark:text-white'>سرویس اطلاعیه</th>
+							<th className='max-md:hidden w-[120px] py-3 px-1 text-center font-medium text-black dark:text-white'>سرویس اطلاعیه</th>
 							{showType === 'all' ? (
 								<th className='max-sm:hidden w-[120px] py-3 px-1 text-center font-medium text-black dark:text-white'>
 									زمان شروع نمایش
